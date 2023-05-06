df8db06d964ce392d3071b4a48e81d03b63ac8ad
Pupa06 <tduy8901@gmail.com>
2023-05-06 12:04:43 +0700

    update

--- demo-huongnghiep-ai/src/components/MessageBlock/MessageBlock.jsx
+++ demo-huongnghiep-ai/src/components/MessageBlock/MessageBlock.jsx
@@ -1,3 +1,4 @@
+import logo from "../../assets/Logo.png";
 import "./styles.css";
 
 const dataQuestions = [
@@ -21,7 +22,26 @@ function MessageBlock() {
             <p>
                 {dataQuestions.map((item, index) => <Questions content={item} key={index} />)}
             </p>
+            <MessageItem />
         </>
     )
 }
 export default MessageBlock
+
+function MessageItem() {
+    return (
+        <>
+            <div className="text-block">
+                <div><img src={logo} alt="logo" /></div>
+                <div className="text">
+                    <p className="text-1">Thiết kế đồ họa là quá trình tạo ra các sản phẩm hình ảnh có tính thẩm mỹ, thông qua sử dụng các công nghệ, phần mềm và kỹ thuật khác nhau. Các sản phẩm đồ họa thường bao gồm đồ họa kỹ thuật số, thiết kế đồ họa cho ấn phẩm, thiết kế trang web, thiết kế đồ hoạ cho sản phẩm quảng cáo, thiết kế đồ họa cho phim, trò chơi và nhiều lĩnh vực khác. Công việc của một nhà thiết kế đồ họa là sáng tạo, đưa ra ý tưởng và chuyển đổi chúng thành các sản phẩm đồ họa đẹp mắt và chuyên nghiệp để phục vụ cho mục đích nào đó của khách hàng.</p>
+                    <a href="#" className="text-2">Tính cách của tôi có phù hợp với ngành Thiết kế đồ hoạ không?</a>
+                    <a href="#" className="text-2">Có thể xây cho tôi bản đồ sự nghiệp cho ngành Thiết kế đồ hoạ không?</a>
+                    <a href="#" className="text-2">Xem các chủ đề gợi ý khác</a>
+                </div>
+            </div>
+        </>
+    )
+}
+
+
