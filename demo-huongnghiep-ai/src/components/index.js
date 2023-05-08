c7d491c919e3ef0a1a12c4c0432ad2721734c661
Pupa06 <tduy8901@gmail.com>
2023-05-09 02:21:48 +0700

    feature header duy

--- demo-huongnghiep-ai/src/components/index.js
+++ demo-huongnghiep-ai/src/components/index.js
@@ -2,11 +2,13 @@ import MessageBlock from "./MessageBlock/MessageBlock";
 import MessageItem from "./MessageItem/MessageItem";
 import MessageInput from "./MessageInput/MessageInput";
 import PrimaryLayout from "./Organisms/layout/PrimaryLayout";
+import SearchBar from "./SearchBar/SearchBar";
 
 
 export {
     MessageBlock,
     MessageItem,
     MessageInput,
-    PrimaryLayout
+    PrimaryLayout,
+    SearchBar,
 }
