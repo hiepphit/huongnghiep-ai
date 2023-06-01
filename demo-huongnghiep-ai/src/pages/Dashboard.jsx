48df2e94f3760b1fde51687e9ca458951b451522
Hiep <72513114+hiepphit@users.noreply.github.com>
2023-06-02 03:11:23 +0700

    Add app source product

--- demo-huongnghiep-ai/src/pages/Dashboard.jsx
+++ demo-huongnghiep-ai/src/pages/Dashboard.jsx
@@ -4,18 +4,17 @@ import { dataQuestions } from "../components/MessageBlock/MessageBlock";
 import "./style.css";
 import { StartRoadmap } from "../components";
 import { TestJohn } from "../components";
+import { useNavigate } from "react-router-dom";
 
 const Questions = ({ content }) => {
-  return (
-    <div className="bg-white">
-      "{content}"
-    </div>
-  )
-}
-
+  return <div className="bg-white">"{content}"</div>;
+};
 
 function Dashboard() {
-
+  const navigate = useNavigate();
+  const handleMessageSubmit = async () => {
+    navigate("/chat");
+  };
   return (
     <div className="my-dashboard">
       <div className="flex-row">
@@ -24,12 +23,22 @@ function Dashboard() {
       </div>
       <div>
         <h1 className="title-content">Gợi ý chủ đề</h1>
-        <a href="/Chat" className="text-content">{dataQuestions.map((item, index) => <Questions content={item} />)}</a>
+        <a href="/Chat" className="text-content">
+          {dataQuestions.map((item, index) => (
+            <Questions content={item} />
+          ))}
+        </a>
       </div>
       <div>
         <MessageInput
+          placeholder={"Viết gì đó ..."}
+          onEnter={handleMessageSubmit}
           rightButton={
-            <Button children="Gửi" type="primary" onClick={() => { }} />
+            <Button
+              children="Gửi"
+              type="primary"
+              onClick={handleMessageSubmit}
+            />
           }
         />
       </div>
